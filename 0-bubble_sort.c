6bec7fe5abb8f8bff7222a3ef1b4a759f4f0cc2b
L344S <7474@holbertonstudents.com>
2024-01-15 18:20:32 +0100

    Updated 0-bubble_sort.c
    Attempt to reach 12/12

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,22 +13,22 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t a, b;
-	size_t store_temporary;
+	int store_temporary;
 
 	/* check if array is NULL or size is less than 2 */
 	if (array == NULL || size < 2)
 		return; /* stop cuz there's nothing to sort */
 
 	/* go through array from 0 to size - 1 */
-	for (a = 0; a < size; a++)
+	for (a = 0; a < size - 1; a++)
 		/* go through array again from 0 to size - 1 */
 		for (b = 0; b < size - 1; b++)
 			/* if current element is greater than next element */
 			if (array[b] > array[b + 1])
 			{
-				store_temporary = array[b];
-				array[b] = array[b + 1];
-				array[b + 1] = store_temporary;
+				store_temporary = array[b + 1];
+				array[b + 1] = array[b];
+				array[b] = store_temporary;
 				/* print array every time we swap */
 				print_array(array, size);
 			}
